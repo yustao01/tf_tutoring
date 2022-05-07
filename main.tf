44672f5f6c7504cc6cd36aaf95dd1160177442ee
Yusuf Akinbade <yustao2002@yahoo.co.uk>
2022-05-07 02:15:12 -0500

    Static website files all included

---
+++ main.tf
@@ -0,0 +1,41 @@
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_s3_bucket" "my_website_domain" {
+  bucket = var.website_domain
+  acl = "public-read"
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [{
+    "Sid": "PublicReadAccessForWebsite",
+    "Effect": "Allow",
+    "Action": "s3:GetObject",
+    "Principal": "*",
+    "Resource": ["arn:aws:s3:::${var.website_domain}/*"]
+    }
+  ]
+}
+POLICY
+website {
+  index_document = "index.html"
+  error_document = "error.html"
+}
+}
+
+resource "aws_s3_bucket_object" "index_html" {
+  bucket = var.website_domain
+  key    = "index.html"
+  content_type = "text/html"
+
+  source = "index.html"
+}
+
+resource "aws_s3_bucket_object" "error_html" {
+  bucket = var.website_domain
+  key    = "error.html"
+  content_type = "text/html"
+
+  source = "error.html"
+}
